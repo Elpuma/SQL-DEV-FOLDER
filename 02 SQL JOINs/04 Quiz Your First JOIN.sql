dd63d17726e0883279c4da3f7c9988bfb403ad69
Jose Rodriguez <darkpumex@gmail.com>
2021-08-22 20:36:11 -0600

    master

--- 02 SQL JOINs/04 Quiz Your First JOIN.sql
+++
@@ -1,7 +0,0 @@
-/*Write Your First JOIN
-Below we see an example of a query using a JOIN statement. Let's discuss what the different clauses of this query mean.*/
-
-SELECT orders.*, accounts.*
-FROM orders
-JOIN accounts
-ON orders.account_id = accounts.id;
